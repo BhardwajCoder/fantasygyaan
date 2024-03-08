6bf9a7d85027ba5e528fc1f22f7996124bbe6094
BhardwajCoder <bhardwajlokesh946@gmail.com>
2024-03-08 08:34:16 +0530

    changes

--- src/Components/Matchdata/Matchdata7.js
+++ src/Components/Matchdata/Matchdata7.js
@@ -8,26 +8,26 @@ function Matchdata7() {
     <>
     <div className='container Match ' style={{fontSize:"1.3rem"}}>
     <Helmet>
-    <title>LAH VS KAR -Dream11 Predictions</title>
-    <meta name="description" content="Check the free Fantasy Predictions and Dream11 Free teams of LAH vs KAR match.Get the Fantasy Bonus Tips to increase your chance of winnings" />
-    <meta name="keywords" content="LAH vs KAR-Dream11 Predictions, Lahore  vs Karachi free fantasy team, LAH vs KAR Today Match predictions for free,LAH vs KAR fantasy tips" />
+    <title>LAH VS KAR Dream11 Predictions,Today Match Prediction</title>
+    <meta name="description" content="Check the free Fantasy Predictions and Dream11 Free teams of LAH vs KAR match.Get the Fantasy Bonus Tips to increase your chance of winnings." />
+    <meta name="keywords" content="LAH vs KAR Dream11 Predictions, Lahore  vs Karachi free fantasy team, LAH vs KAR Today Match predictions,lah vs kar today match  ,lah vs kar dream11 prediction" />
     <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
     </Helmet>
-      <h1 className='text-center'>LAH vs KAR |Dream11 Prediction</h1>
+      <h1 className='text-center'>LAH vs KAR Dream11 Prediction,today Match prediction</h1>
       <img src="https://www.probatsman.com/wp-content/uploads/2024/02/Match-10-LAH-vs-KAR-Pakistan-Super-League-2024.jpg" alt="BAN-W vs UP-W
        match Predictions "  className='float-end match-img'/>
-      <p>The Match will play between Lahore and Karachi both team put their efforts to win .Lahore loose all their 3 matches and on the other side Karch with 1win or 1 loss so we will see a intersting match because both teams want win and lahore definitely need a win to start their Campaign this season so be ready at toss time for toss and official pitch report so stay tuned . </p>
+      <p>The Match will play between Lahore and Karachi both team put their efforts to win .this season performance of the both teams was dissapointed.In this season KAR won only 3 matches out of 8 matches and currently hold the 5th position on the points table and on the other side LAH at the bottom of the points table they only won 1 match out of 8 matches.Both teams have to step up in this match because both teams need a victory to bounce back in the tournament so let`s see what happens in this match stay tuned at toss time for updates  . </p>
 
-      <h2>LAH vs KAR Match predictions</h2>
+      <h2>LAH vs KAR Dream11 Prediction, Today Match predictions</h2>
      
      <h2>Pitch Report</h2>
-     <p>This match will play on Gaddafi Stadium Lahore the Nature of the pitch is Good Surface to bat on,has generally been neutal during the international games but In PSL people want entertainment so they prepare Batting Friendly Tracks so this is going to another High scoring match so pick Quality Batsmen death bowlers or all-rounder who can bat in top order.</p>
+     <p>This match will play on National Stadium Karachi the Nature of the pitch is <strong> Good Surface to bat on</strong>,has generally been neutral during the international games but In PSL people want entertainment so they prepare Batting Friendly Tracks so this is going to another High scoring match so pick Quality Batsmen death bowlers or all-rounder who can bat in top order.</p>
 
      <h3> Lah Probable 11</h3>
-     <p>S Farhan(wk),Fakhar Zaman,R vd Dussen,Kamran Ghulam,Sikandar Raza,C Brathwaite,Jahandad Khan,George Linde,Saheen Afridi,Salman Fayyaz,Zaman Khan</p>
+     <p>S Farhan,Fakhar Zaman,R vd Dussen,Shai Hope(wk),Sikandar Raza,Ahsan Hafeez,Jahandad Khan,David wiese,Saheen Afridi,T Abbas,Zaman Khan</p>
      <br />
      <h3>KAR Probable 11</h3>
-     <p>Shan Masood,Mohd Akhliq,James Vince,Shoaib Malik,Mohd Nawaz,Kieron Pollard,Irfan Khan,Danie Sams,Hasan Ali,Mir Hamza,Tabraiz Shamsi</p>
+     <p>Shan Masood,T Seifert(wk),James Vince,Shoaib Malik,Mohd Nawaz,Kieron Pollard,Irfan Khan,Z Mahmood,Hasan Ali,Mir Hamza,Blessing Muzurbani</p>
      </div>
      <div className='container' style={{fontSize:"1.3rem"}}>
 
@@ -42,7 +42,7 @@ function Matchdata7() {
      <ol>
      <li><strong>Sikandar Raza</strong>- will be the best pick in all-rounder section</li>
       <li><strong>Shoaib Malik</strong>- he can give points both with bat and ball</li>
-      <li><strong>Daniel Sams</strong>- he can give points both with bat and ball</li>
+      <li><strong>David Wiese</strong>- he can give points both with bat and ball</li>
       <li><strong>M Nawaz</strong> will be good pick beacause he can bowl some overs</li>
       
      </ol>
@@ -56,16 +56,22 @@ function Matchdata7() {
      <h4>Top Captaincy picks</h4>
      <ol>
       <li><strong>Sikandar Raja</strong> can contribute with bat and ball</li>
-      <li><strong>Daniel Sams</strong>can contribute with bat and ball- Risky </li>
-      <li><strong>Shaeen Afridi</strong>safe pick but you can try</li>
+      <li><strong>Mohammad Nawaz</strong>can contribute with bat and ball- Risky </li>
+      <li><strong>Shaeen Afridi</strong>safe - you can try</li>
      </ol>
      <h5>Fantasy Bonus tips</h5>
      <ol>
-      <li>You can Try George Linde</li>
-      <li>You can Try Abdullah Shafique bat First</li>
+      <li>You can Try Kieron Pollard if bat first-good trump pick</li>
+      <li>Make Saheen Afirdi captain in 1st Inning due to death bowling </li>
       <li>You can Drop Fakhar Zaman his form is not good</li>
      </ol>
      <h6>Check out the final team after toss- <Link to='https://www.instagram.com/fantasygyaan9/'>  <FaInstagram size={"1.5rem"}/> </Link> </h6>
+     <h6>FAQ</h6>
+     <ol>
+     <a href="/what-is-fantasyCricket"><li>what is fantasy Cricket</li></a>
+      <a href="/Why-We-Should-Play-Fantasy-Cricket"><li>WHY We Should Play Fantasy Cricket</li></a>
+      <a href="/How-to-Earn-Money-from-FantasyCricket"><li>How to Earn Money from Fantasy Cricket</li></a>
+     </ol>
      </div>
      </>
   )
