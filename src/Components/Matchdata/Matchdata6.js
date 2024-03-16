70fda98cf8d5afa24205da1eca77da7dec3caff5
BhardwajCoder <bhardwajlokesh946@gmail.com>
2024-03-16 09:14:16 +0530

    changes

--- src/Components/Matchdata/Matchdata6.js
+++ src/Components/Matchdata/Matchdata6.js
@@ -9,18 +9,18 @@ function Matchdata6() {
     <div className='container Match ' style={{fontSize:"1.3rem"}}>
     <Helmet>
     <title> rcb vs csk dream11 prediction today match - IPL 2024 Match1 </title>
-    <meta name="description" content="Check the free Fantasy Predictions and Dream11 Free teams of BAN-W vs UP-W match.Get the Fantasy Bonus Tips to increase your chance of winnings" />
+    <meta name="description" content="Check the free Dream11 Predictions and fantasy tips  of rcb vs csk match.This is the 1st match of ipl 2024 will play between rcb vs csk.Both teams have huge fanbase and this is going to be an action pack show for audience." />
     <meta name="keywords" content="rcb vs csk,RCB vs CSK,rcb vs csk dream11 prediction,rcb vs csk today match prediction" />
     <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
     </Helmet>
       <h1 className='text-center'>rcb vs csk dream11 prediction today match-2024 </h1>
       <img src="https://d16f573ilcot6q.cloudfront.net/wp-content/uploads/2023/04/WhatsApp-Image-2023-04-16-at-8.57.35-AM.jpeg" alt=" "  className='float-end match-img'/>
-      <p>This is the 1st match of ipl 2024 will play between rcb vs csk.Both teams have huge fanbase and this is going to be an action pack show for audience because King Kholi and the MS Dhoni will play and people love the both cricketers and this is the home ground of csk so this is the good chance for csk to  start their camapign with home ground and on the other side rcb ready to get their 1st win in this season so this is going to be a action or entertainment pack for cricket fans. </p>
+      <p>This is the 1st match of ipl 2024 will play between rcb vs csk.Both teams have huge fanbase and this is going to be an action pack show for audience because King Kholi and the MS Dhoni will play and people love the both cricketers and this is the home ground of csk so this is the good chance for csk to  start their campaign with home ground and on the other side rcb ready to get their 1st win in this season so this is going to be a action or entertainment pack for cricket fans. </p>
 
       <h2>RCB vs CSK Today Match predictions</h2>
      
      <h2>Pitch Report:</h2>
-     <p>The match will play on  MA Chidambaram Stadium Chennai the nature of the pitch is <strong>Batting in 1st inning is good and 2nd inning spinners will key</strong> Batters will take advantage in 1st inning with the bat because in 1st inning pitch is fresh but with the match progress pitch start helping the spinners in 2nd innings in chennai but overall balanced track.stay tuned at toss time for afficial pitch report.  .</p>
+     <p>The match will play on  MA Chidambaram Stadium Chennai the nature of the pitch is <strong>Batting in 1st inning is good and 2nd inning spinners will key</strong> Batters will take advantage in 1st inning with the bat because in 1st inning pitch is fresh but with the match progress pitch start helping the spinners in 2nd innings in chennai but overall balanced track.stay tuned at toss time for official pitch report.  .</p>
      <h3>RCB playing X1:</h3>
      <p>Faf du Plessis,Virat Kholi,Rajat Patidar,Glenn Maxwell,Cameron Green,Dinesh Karthik(wk),Mayank Dagar,Mohammad Siraj,Reece Topley,Alzari Joseph,Akash Deep,Vijaykumar Vyshak</p>
      <br />
