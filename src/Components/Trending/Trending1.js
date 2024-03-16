c5b4dce092e1c471e8685cc4b4cc3d47f971aeca
BhardwajCoder <bhardwajlokesh946@gmail.com>
2024-03-16 15:45:23 +0530

    changes

--- src/Components/Trending/Trending1.js
+++ src/Components/Trending/Trending1.js
@@ -8,20 +8,23 @@ function Trending1() {
        <Helmet>
         <title>What is Fantasy Cricket</title>
         <meta name="description" content="If you want to know play Fantasy cricket so check here What is Fantasy Cricket and how to play fantasy cricket before start playing you have to know all details about Fantasy Cricket it is very important otherwise you will lose your money." />
-        <meta name="keywords" content=" What is Fantasy cricket, How to play fantasy cricket" />
+        <meta name="keywords" content=" what is fantasy cricket, How to play fantasy cricket" />
         <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
       </Helmet>
       <h1 className='text-center my-3'>What is Fantasy Cricket</h1>
         <img src="/images/FANTASY.png" alt=" what is fantasy cricket" className='float-end img-trend' />
       <p><strong>Fantasy Cricket </strong>is the fusion of strategy,knowledge and passion for cricket .where user can create their own team for the match and these players chosen by both teams that playing cricket on that match user have to choose 11 players for their team and the player`s performance of these players in actual matches determines the fantasy team's success. Users choose their team captain and vice captain . the captain of the team 2x of the actual point scored by the player same as vice captain 1.5x of the points . In some matches  right captain and the vice captain decide your winnings.this is the fantasy cricket and you can earn real money from that by playing different contest on fantasy app platforms or  but there are many points keep in the mind for making your fantasy team for the matches like Pitch report, Players form, Match up and Venue score etc.</p>
       <h2>How to play Fantasy Cricket</h2>
-      <p>If you want to play fantasy cricket . you have to choose a fantasy app platform to create your fantasy team and after that you have to join the contests. choose your fantasy app platform carefully there are many fantasy apps in the market so check the app before registration. In the market many apps offer you Bonus cash and Real cash for playing your first contest for  free so choose your app and when the match is completed you check your result in the fantasy app.</p>
-      <h3>choose Fantasy app</h3>
-      <p> you have to choose your fantasy app first.there are lots of fantasy app in the market  so choose your fantasy app. When you choose your fantasy app you have to check some important features like checking if your app  supports 7+4 combination or 10+1 combination. check your fantasy app is supporting Paytm withdrawal or not or check the TDS policy because according the government policy you have to pay 28% TDS on positive net winnings.check your fantasy app is giving Bonus cash or Real cash you have to join the app that giving Real cash because you can use Real cash for playing your first contest free</p>
-      <h4>Create fantasy team</h4>
-      <p>You have to create your fantasy team for playing fantasy cricket, create your 11 players team and choose one Captain and vice captain from the 11 players you selected in your team. when  match is completed check your fantasy team score.if your fantasy team score more points from your opponent so you can earn real money</p>
-      <h5>Add money to wallet</h5>
-      <p>for playing fantasy cricket you have to add money to your fantasy account wallet but invest your money carefully.after you add money to your  wallet you can easily join multiple contests but this games involve financial risk so invest your money Carefully.</p>
+      <p>If you want to play fantasy cricket .  <strong> you have to choose a fantasy app platform to create your fantasy team and after that you have to join the contests. choose your fantasy app platform carefully there are many fantasy apps in the market so check the app before registration.</strong> In the market many apps offer you Bonus cash and Real cash for playing your first contest for  free so choose your app and when the match is completed you check your result in the fantasy app.</p>
+      
+      <h3>Things you need to start fantasy Cricket</h3>
+      <ol>
+        <li> <strong>Fantasy app</strong></li>
+        <p>Choose fantasy apps according to your requirements and facilities you have to check that which fantasy apps gives you <strong>paytm withdrawl or Bank Withdrawl, 100% Bonus amount use, tds or gst free on net winnings,Contest variety according to your budget. </strong> so check these things that which apps provide you best offers and services.</p>
+        <li><strong>Create Fantasy teams</strong></li>
+        <p>You have to create your fantasy team.you have to select 11 palyers from 22 players who will play in that match.for selecting players you have to do the analysis of match like <strong>Players recent form,pitch report,venue score and players matchups </strong> you have to analysis multiple things for creating your fantasy team</p>
+      </ol>
+     
       <h5><strong>Conclusion</strong></h5>
       <p>Fantasy cricket is the fusion of strategy,knowledge and passion for cricket.where we can create fantasy teams for the real matches played by real cricketers and the concept of fantasy cricket gives you a experience that you play cricket in real match time and give a chance to use your cricket analysis,skills and cricket knowledge to create fantasy team and earn real money from that.you can create team using fantasy apps platforms and join the contest you want.but you have to careful with that when you start playing after some time you can addict with this so play carefully at your own risk.   </p>
       <h6>FAQ</h6>
