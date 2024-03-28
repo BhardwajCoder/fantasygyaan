05527c032a30dfdd5c12fa99ec5f5790ddbb8d60
BhardwajCoder <bhardwajlokesh946@gmail.com>
2024-03-28 14:53:40 +0530

    changes done

--- src/Components/Matchdata/Matchdata6.js
+++ src/Components/Matchdata/Matchdata6.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Match.css'
 import { FaInstagram } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 function Matchdata6() {
+  const [sections] = useState([
+    { id: 'section1', title: 'Head To Head Records' },
+    { id: 'section2', title: 'Pitch Report' },
+    { id: 'section3', title: 'RCB vs KKR Playing11' },
+    { id: 'section4', title: 'RCB vs KKR Dream11 Team' },
+    { id: 'section5', title: 'Top Batting Picks' },
+    { id: 'section6', title: 'Top All-Rounder Picks' },
+    { id: 'section7', title: 'Top Bowling Picks' },
+    { id: 'section8', title: 'RCB vs KKR Fantasy Tips' },
+    
+  ]);
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <>
-    <div className='container Match ' style={{fontSize:"1.4rem"}}>
+    <div className='Match container style' >
     <Helmet>
     <title> RCB vs KKR Dream11 Prediction Today Match 10 IPL 2024 </title>
-    <meta name="description" content="Check the free Dream11 Predictions and fantasy tips  of rcb vs csk match.This is the 1st match of ipl 2024 will play between rcb vs csk.Both teams have huge fanbase and this is going to be an action pack show for audience." />
-    <meta name="keywords" content="RCB vs KKR Dream11 Prediction,RCB vs CSK  Today Match,RCB vs KKR Dream11 Prediction Today Match,rcb vs csk dream11 prediction,RCB vs KKR Pitch Report" />
+    <meta name="description" content="Check the free Dream11 Predictions and fantasy tips of rcb vs csk match.This is the 1st match of ipl 2024 will play between rcb vs csk.Both teams have huge fanbase and this is going to be an action pack show for audience." />
+    <meta name="keywords" content="RCB vs KKR Dream11 Prediction,RCB vs KKR Today Match,RCB vs KKR Dream11 Prediction Today Match,rcb vs kkr dream11 prediction,RCB vs KKR Dream11 Teams" />
     <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
     </Helmet>
-      <h1 className='text-center'>RCB vs KKR Dream11 Prediction,Today Match 10 IPL 2024</h1>
+      <h1 className='my-2 heading-style'>RCB vs KKR Dream11 Prediction,Today Match 10 IPL 2024</h1>
       <img src="/images/rcbvskkrposter.jpg" alt="RCB vs KKR Dream11 Prediction Today Match IPL 2024 "  className='float-end match-img'/>
       <ul>
         <li>
@@ -22,9 +39,20 @@ function Matchdata6() {
         <li>
         <p> RCB played 2 matches in this season with 1 loss and 1 victory and on the other side KKR won their 1st match against SRH by 4 runs.Both teams want to win to boost up their confidence let`s see what happens stay tuned for the official updates and team news at toss time.</p>
         </li>
+      </ul> 
+      <h2 className='heading-style'>Table of content</h2>
+     <div className='table-content container-fluid'>
+      <ul>
+        {sections.map(section => (
+          <li key={section.id} className='py-1 my-2'>
+            <a href={`#${section.id}`} onClick={() => scrollToSection(section.id)}>
+              {section.title}
+            </a>
+          </li>
+        ))}
       </ul>
-  
-     
+     </div>
+      <section id='section1'className='my-3'>
       <h2 className='heading-style'> RCB vs KKR Head to Head Records</h2>
      <div>
      <table class="table">
@@ -58,6 +86,8 @@ function Matchdata6() {
   </tbody>
 </table>
      </div>
+     </section>
+     <section id='section2' className='my-3'>
      <h2 className='heading-style'>RCB vs KKR Pitch Report</h2>
      <p>The match betwwen RCB vs KKR play at <strong> M Chinawsamy Stadium,Banglore</strong>.The nature of the pitch is good batting wicket .this is the 2nd match on this ground so you can expect good batting wicket.stay tuned for official pitch report at toss time. </p>
      <div>
@@ -81,24 +111,29 @@ function Matchdata6() {
     
 </table>
      </div>
-      <h2>RCB vs KKR Squad:</h2>
+     </section>
+     <section id='section3'>
+      <h2 className='heading-style'>RCB vs KKR Squad:</h2>
       <ol>
-        <li> <strong>RCB Playing 11:</strong><span> <p>Faf du Plessis,Virat Kholi,Rajat Patidar,Glenn Maxwell,Cameron Green,Dinesh Karthik(wk),Mayank Dagar,Mohammad Siraj,Reece Topley,Alzari Joseph,Akash Deep,Vijaykumar Vyshak</p></span> </li>
-        <li> <strong>KKR Playing 11: </strong><span><p>Phil Salt,Sunil Naraine,Venkatesh Iyer,Shreyas Iyer,Nitish Rana,Ramandeep Singh,Rinku Singh,Andre Russell,Mitchell Starc,Harshit Rana,Varun Chakaravarthy,Suyesh Sharma</p></span> </li>
+         <p style={{letterSpacing:"1px"}}><strong>RCB Playing 11:</strong> Faf du Plessis,Virat Kholi,Rajat Patidar,Glenn Maxwell,Cameron Green,Dinesh Karthik(wk),Mayank Dagar,Mohammad Siraj,Reece Topley,Alzari Joseph,Akash Deep,Vijaykumar Vyshak</p>
+       <p style={{letterSpacing:"1px"}}><strong>KKR Playing 11: </strong>Phil Salt,Sunil Naraine,Venkatesh Iyer,Shreyas Iyer,Nitish Rana,Ramandeep Singh,Rinku Singh,Andre Russell,Mitchell Starc,Harshit Rana,Varun Chakaravarthy,Suyesh Sharma</p>
       </ol>
+      </section>
      </div>
-     <div className='container' style={{fontSize:"1.4rem"}}>
-      <div className='container'>
-      <h3>RCB vs KKR Fantasy Team No 1</h3>
-    <img src="/images/rcbvskkr.jpeg" alt="RCB vs KKR Fantasy Team " className=' team-img ' />
-      </div>
-      <h3 className='my-3 heading-style'>Top Batting Picks:</h3>
+     <div className='container style' >
+     <section id='section4'> <h3 className='heading-style'>RCB vs KKR Dream11 Team</h3>
+    <img src="/images/rcbvskkr.jpeg" alt="RCB vs KKR Dream11 Team " className=' team-img ' /></section>
+     
+     <section id='section5'>
+     <h3 className='my-3 heading-style'>Top Batting Picks:</h3>
      <ol>
      <li> <strong>Virat Kholi</strong>  - good recent form and venue stats</li>
      <li> <strong>Faf Du Plesis</strong>- good recent form and venue stats</li>
      <li> <strong>Phil Salt</strong> - good batsmen you can try </li>
      <li><strong>Shreyas Iyer</strong>- Good Batsmen </li>
      </ol>
+     </section>
+     <section id='section6'>
      <h4 className='heading-style'>Top All-Rounder Picks:</h4>
      <ol>
      <li><strong>Glen Maxwell</strong>- will be the best pick in all-rounder section</li>
@@ -107,7 +142,9 @@ function Matchdata6() {
       <li><strong>Sunil Naraine</strong>- will be good pick beacause he can bowl some overs</li>
       
      </ol>
-     <h4 className='heading-style'>Top Bowling Picks:</h4>
+     </section>
+    <section id='section7'>
+    <h4 className='heading-style'>Top Bowling Picks:</h4>
      <ol>
       <li><strong>Mitchell Starc </strong>- if RCB bowl first.</li>
       <li><strong>Harshit Rana </strong>- you can try in both innings</li>
@@ -115,6 +152,8 @@ function Matchdata6() {
       <li><strong>Alzari Joseph</strong>- good recent form and venue stats</li>
       
      </ol>
+    </section>
+     
      <h4 className='heading-style'>Top Captaincy picks:</h4>
      <ol>
       <li><strong>Glen Maxwell</strong>- can contribute with bat and ball</li>
@@ -122,12 +161,15 @@ function Matchdata6() {
       <li><strong>Andre Russell</strong>- safe pick but you can try</li>
       <li><strong>Virat Kholi</strong>- risky captain option pick but you can try</li>
      </ol>
+     <section id='section8'>
      <h5 className='heading-style'>Fantasy Bonus tips:</h5>
      <ol>
       <li>you can try Dinesh Karthik in 1st innings</li>
       <li>you can take M Siraj in 1st  inning</li>
       <li>you can try Suyesh Sharma in your team.</li>
      </ol>
+     </section>
+    
      <h6 className='heading-style'>Check out the final team after toss- <Link to='https://www.instagram.com/fantasygyaan9/'>  <FaInstagram size={"1.5rem"}/> </Link> </h6>
      
      </div>
